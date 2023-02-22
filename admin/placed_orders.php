57f24b97bad46efe697f138cb05b7318e9a18f9d
quelzarth <brosennjeimar@yahoo.com>
2023-02-23 02:57:50 +0800

    basta update, ewan ko kung ano

--- admin/placed_orders.php
+++ admin/placed_orders.php
@@ -14,14 +14,14 @@ if(isset($_POST['update_payment'])){
    $order_id = $_POST['order_id'];
    $payment_status = $_POST['payment_status'];
    $payment_status = filter_var($payment_status, FILTER_SANITIZE_STRING);
-   $update_payment = $conn->prepare("UPDATE `orders` SET payment_status = ? WHERE id = ?");
+   $update_payment = $conn->prepare("UPDATE `bookings` SET payment_status = ? WHERE id = ?");
    $update_payment->execute([$payment_status, $order_id]);
    $message[] = 'payment status updated!';
 }
 
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
-   $delete_order = $conn->prepare("DELETE FROM `orders` WHERE id = ?");
+   $delete_order = $conn->prepare("DELETE FROM `bookings` WHERE id = ?");
    $delete_order->execute([$delete_id]);
    header('location:placed_orders.php');
 }
@@ -34,7 +34,7 @@ if(isset($_GET['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>placed orders</title>
+   <title>placed bookings</title>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
@@ -47,12 +47,12 @@ if(isset($_GET['delete'])){
 
 <section class="orders">
 
-<h1 class="heading">placed orders</h1>
+<h1 class="heading">placed bookings</h1>
 
 <div class="box-container">
 
    <?php
-      $select_orders = $conn->prepare("SELECT * FROM `orders`");
+      $select_orders = $conn->prepare("SELECT * FROM `bookings`");
       $select_orders->execute();
       if($select_orders->rowCount() > 0){
          while($fetch_orders = $select_orders->fetch(PDO::FETCH_ASSOC)){
@@ -62,8 +62,8 @@ if(isset($_GET['delete'])){
       <p> name : <span><?= $fetch_orders['name']; ?></span> </p>
       <p> number : <span><?= $fetch_orders['number']; ?></span> </p>
       <p> address : <span><?= $fetch_orders['address']; ?></span> </p>
-      <p> total products : <span><?= $fetch_orders['total_products']; ?></span> </p>
-      <p> total price : <span>$<?= $fetch_orders['total_price']; ?>/-</span> </p>
+      <p> services : <span><?= $fetch_orders['total_products']; ?></span> </p>
+      <p> total price : <span>₱<?= $fetch_orders['total_price']; ?></span> </p>
       <p> payment method : <span><?= $fetch_orders['method']; ?></span> </p>
       <form action="" method="post">
          <input type="hidden" name="order_id" value="<?= $fetch_orders['id']; ?>">
@@ -81,7 +81,7 @@ if(isset($_GET['delete'])){
    <?php
          }
       }else{
-         echo '<p class="empty">no orders placed yet!</p>';
+         echo '<p class="empty">no bookings placed yet!</p>';
       }
    ?>
 
