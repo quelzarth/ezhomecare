c46e0fe725423709d8659a9b78766d8ce8b78318
quelzarth <brosennjeimar@yahoo.com>
2023-02-27 01:44:14 +0800

    update ulit

--- components/footer.php
+++ components/footer.php
@@ -4,9 +4,8 @@
 
       <div class="box">
          <h3>quick links</h3>
-         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
          <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
-         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
          <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
       </div>
 
@@ -27,11 +26,12 @@
 
       <div class="box">
          <h3>follow us</h3>
-         <a href="https://www.facebook.com/ezhomecareph" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://www.facebook.com/ezhomecareph" target="_blank"><i class="fab fa-facebook-f"></i>Facebook</a>
       </div>
 
    </section>
 
-   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>ByteMe Group</span> | all rights reserved!</div>
-
+   <div class="credit">Created by <span>ByteMe</span>
+      <?= date('Y'); ?>
+   </div>
 </footer>
